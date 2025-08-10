872c04f5d7353da10df2363c16d2871cdf665af1
Matt Hendrie <matthendrie@protonmail.com>
2025-08-10 21:57:40 +1000

    Started getting HTMX working

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -9,4 +9,12 @@ module.exports = {
     clean: true,
     filename: './js/app.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
--- frontend/webpack.config.dev.js
+++ frontend/webpack.config.dev.js
@@ -9,5 +9,24 @@ module.exports = merge(common, {
     hot: true,
     open: true,
     static: ['./'],
+    proxy: [
+      {
+        context: ['/api'],
+        target: 'http://localhost:8000',
+        pathRewrite: { '^/api': '' },
+        changeOrigin: true,
+        secure: false,
+        logLevel: 'debug',
+        onProxyReq: (proxyReq, req, res) => {
+          console.log('Proxying request:', req.method, req.url, '→', proxyReq.path);
+        },
+        onProxyRes: (proxyRes, req, res) => {
+          console.log('Received response:', proxyRes.statusCode, 'for', req.url);
+        },
+        onError: (err, req, res) => {
+          console.error('Proxy error:', err);
+        }
+      }
+    ],
   },
 });
